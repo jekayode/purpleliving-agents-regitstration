8c82ce149a52259d6a922bf04c83cd1931946ef0
Godfred Akpan <godfredakpan@gmail.com>
2023-03-30 15:21:17 +0100

    update UI

--- src/components/registration.js
+++ src/components/registration.js
@@ -45,7 +45,6 @@ function Registration() {
       return;
     }
 
-
     const data = {
       ...formState,
     }
@@ -100,11 +99,10 @@ function Registration() {
   // console.log(formState);
 
   return (
-    <div class="container" style={{marginTop: '100px', marginBottom: '100px'}}>
+    <div class="container" style={{marginTop: '50px', marginBottom: '50px'}}>
+      <img src="https://content.app-sources.com/s/19193257581773851/uploads/Images/Purple_Logo_Trans-1266811.png" alt="logo" style={{width: '100px', display: 'block', margin: 'auto', marginBottom: '10px'}} />
 
-      <h1 class="well text-center">Registration Form</h1>
-
-      <div class="col-lg-10 well card mx-auto">
+      <div class="col-lg-6 well card mx-auto">
         <div class="row card-body">
 
           <input type="hidden" name="reference" value="372789" />
@@ -115,65 +113,66 @@ function Registration() {
             )}
 
           <div class="col-sm-12">
+          <h5>Personal Details</h5>
             <div class="row">
               <div class="col-sm-4 form-group">
-                <label>First Name</label>
+                <label>First Name:</label>
                 <input type="text" name="fname" value={formState.fname} onChange={updateForm} required="required" placeholder="Enter First Name Here.." class="form-control" />
               </div>
               <div class="col-sm-4 form-group">
-                <label>Middle Name</label>
+                <label>Middle Name:</label>
                 <input type="text" name="mname" value={formState.mname} onChange={updateForm} placeholder="Enter Middle Name Here.." class="form-control" />
               </div>
               <div class="col-sm-4 form-group">
-                <label>Last Name</label>
+                <label>Last Name:</label>
                 <input type="text" name="lname" value={formState.lname} onChange={updateForm} required="required" placeholder="Enter Last Name Here.." class="form-control" />
               </div>
             </div>
 
             <div class="row">
               <div class="col-sm-6 form-group">
-                <label>Gender</label>
+                <label>Gender:</label>
                 <select class="form-control" value={formState.gender} onChange={updateForm} name="gender" required="required" >
-                  <option value="">Choose</option>
+                  <option value="">Select Gender</option>
                   <option> Male </option>
                   <option> Female </option>
                 </select>
               </div>
               <div class="col-sm-6 form-group">
-                <label>Date of Birth</label>
+                <label>Date of Birth:</label>
                 <input type="text" name="dob" value={formState.dob} onChange={updateForm} placeholder="Enter Date of Birth Here.." class="form-control" required />
               </div>
             </div>
 
             <div class="row">
               <div class="col-sm-6 form-group">
-                <label>Email Address</label>
+                <label>Email Address:</label>
                 <input type="email" name="email" value={formState.email} onChange={updateForm} placeholder="Enter Email Address Here.." class="form-control" required />
               </div>
               <div class="col-sm-6 form-group">
-                <label>Whatsapp Number </label>
+                <label>Whatsapp Number :</label>
                 <input type="Number" name="phone" value={formState.phone} onChange={updateForm} placeholder="Enter Whatsapp number Here.." class="form-control" required />
               </div>
             </div>
 
             <div class="form-group">
-              <label>Contact Address</label>
+              <label>Contact Address:</label>
               <textarea onChange={updateForm} value={formState.address} placeholder="Enter Contact Address Here.." rows="3" class="form-control" name="address" required></textarea>
             </div>
 
 
-            <h3 class=" ">Bank Details</h3>
+            <h5>Bank Details</h5>
 
             <div class="form-group">
-              <label>Bank Name</label>
+              <label>Bank Name:</label>
               <input type="text" value={formState.bank_name} onChange={updateForm} placeholder="Enter Bank Name Here.." class="form-control" name="bank_name" required />
             </div>
             <div class="form-group">
-              <label>Account Name</label>
+              <label>Account Name:</label>
               <input type="text" value={formState.account_name} onChange={updateForm} placeholder="Enter Account Name Here.." class="form-control" name="account_name" required />
             </div>
             <div class="form-group">
-              <label>Account Number</label>
+              <label>Account Number:</label>
               <input type="text" value={formState.account_number} onChange={updateForm} placeholder="Enter Account Number Here.." class="form-control" name="account_number" required />
             </div>
             <button  onClick={saveInfo} name="register-form" class="btn btn-md btn-success">Submit</button>
@@ -181,7 +180,7 @@ function Registration() {
 
           </div>
           <p>&nbsp;</p>
-          <p style={{margin: "0px 15px 10px"}}>Kindly confirm that the phone number you provided is a whatsapp number. You will be added to a whatsapp group with one week of your registration.</p>
+          <p className=' text-center' style={{ fontWeight: 'bold', color: '#282c3445'}}>Kindly confirm that the phone number you provided is a whatsapp number. You will be added to a whatsapp group with one week of your registration.</p>
 
 
         </div>
