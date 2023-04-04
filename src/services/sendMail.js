3d4899a1cceaabb65383bec0117147d62d338be2
Godfred Akpan <godfredakpan@gmail.com>
2023-04-04 23:44:59 +0100

    update changes

--- src/services/sendMail.js
+++ src/services/sendMail.js
@@ -1,14 +1,17 @@
 import request from 'umi-request';
 
 const API_URL = 'https://invest.purple.xyz/api/v1';
+// local
+// const API_URL = 'https://purple-ipoffer.test/api/v1';
 
 export async function sendMail(body) {
   return request(`${API_URL}/onboard/send-account-mail`, {
-    method: 'get',
+    method: 'post',
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': '*',
     },
     body: JSON.stringify(body),
   });
